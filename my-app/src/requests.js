31a4a0d688eea7afc25d9a970c8a77395ab28cc8
Muaad2001 <98790792+Muaad2001@users.noreply.github.com>
2022-12-04 22:56:08 -0500

    added _

--- my-app/src/requests.js
+++ my-app/src/requests.js
@@ -1,4 +1,4 @@
-const APIKEY = "ce17d631f999e4c131077cd8ee3b120c";
+const API_KEY = "ce17d631f999e4c131077cd8ee3b120c";
 
 const requests = {
     fetchTrending: '/trending/all/week?api_key=${API_KEY}&language=en-US',
